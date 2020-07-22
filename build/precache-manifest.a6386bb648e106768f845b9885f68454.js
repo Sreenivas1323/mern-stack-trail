657b1c7334a0389c2059a7f34c2b88488f046599
Sai Sreenivas <50963808+Sreenivas1323@users.noreply.github.com>
2020-07-23 03:19:29 +0530

    final

---
+++ build/precache-manifest.a6386bb648e106768f845b9885f68454.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb1128ed426bbbae25002ce805497af5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5f4d09f6eabd5ecbd618",
+    "url": "/static/css/2.d98961f1.chunk.css"
+  },
+  {
+    "revision": "5f4d09f6eabd5ecbd618",
+    "url": "/static/js/2.ddd8e9de.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/static/js/2.ddd8e9de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b148a4ac2dd5f84a6352",
+    "url": "/static/js/main.ead31e87.chunk.js"
+  },
+  {
+    "revision": "ba9d6c333b05beccea2b",
+    "url": "/static/js/runtime-main.460028b8.js"
+  }
+]);
